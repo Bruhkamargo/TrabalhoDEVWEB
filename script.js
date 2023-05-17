2fb567f04e3692ea6ec16c0bcf17b4d376a7d51d
bruhkamargo <wwwbrunocamargo@hotmail.com>
2023-05-17 17:05:55 -0300

    Validadores

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-/*const Btn01 = document.getElementById("Btn01")
-Btn01.addEventListener('click', () => {
-    alert()
-})
-*/
 
-const Btn02 = document.getElementById("Btn02")
-Btn02.addEventListener('click', () => {
-    window.location.href = "../index.html"
-})
+function ValidadorCVCadastros() {
+    let Nome = document.getElementById('CadNome');
+    let Email = document.getElementById('CadEmail');
+    let Senha = document.getElementById('CadSenha');
+    if (Nome.value == "" && Email.value == "" && Senha.value == "") {
+        alert("Por favor preencha todosos campos")
+        Nome.focus()
+    } else if (Nome.value == "" || Email.value == "" || Senha.value == "") {
+        if (Nome.value == "") {
+            alert("Por favor defina um Nome valido")
+            Nome.focus()
+        }
+        if (Email.value == "") {
+            alert("Por favor defina um E-mail valido")
+            Email.focus()
+        }
+        if (Senha.value == "") {
+            alert("Por favor defina uma Senha valida")
+            Senha.focus()
+        }
+    } else {
+        alert("Cadastrado!")
+    }
+}
 
 function SaveCadastro() {
     var JsonCadastro = {
@@ -18,6 +33,46 @@ function SaveCadastro() {
 
     localStorage.setItem(0, JSON.stringify(JsonCadastro))
 }
+
 function teste(params) {
     alert("Você foi alertado!")
+}
+
+function ValidadorCampoVazio() {
+    let Email = document.getElementById('EmailLogin')
+    let Senha = document.getElementById('SenhaLogin')
+    if (Email.value == "" || Senha.value == "") {        
+        alert("Por favor preencha todos os campos")
+    } else {
+        if (Email.value == "") {
+            alert("Por favor preencha o seu E-mail")
+            Email.focus()
+        }
+        if (Senha.value == "") {
+            alert("Por favor preencha a sua Senha")
+            Senha.focus()
+        }        
+    }
+}
+
+function ValidadorEmail(IdEmail) {
+    email = document.getElementById(`${IdEmail}`)
+    usuario = email.value.substring(0, email.value.indexOf("@"));
+    dominio = email.value.substring(email.value.indexOf("@") + 1, email.value.length);
+
+    if ((usuario.length >= 1) &&
+        (dominio.length >= 3) &&
+        (usuario.search("@") == -1) &&
+        (dominio.search("@") == -1) &&
+        (usuario.search(" ") == -1) &&
+        (dominio.search(" ") == -1) &&
+        (dominio.search(".") != -1) &&
+        (dominio.indexOf(".") >= 1) &&
+        (dominio.lastIndexOf(".") < dominio.length - 1)) {
+        alert("E-mail valido");
+    }
+    else {
+        alert("E-mail invalido");
+        email.value = ''
+    }
 }
